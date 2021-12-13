9c4e84da06f71aaf9a8ef877b3ec5bf80f5c4f39
designeralabi <bdlkrmalabi@gmail.com>
2021-12-12 20:56:16 -0800

    comment well documented in js

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-// generate a random number
-// link the random number to a string
-// select the image
+// select the h1 tag and store in heading variable
 const heading = document.querySelector('h1');
-
+// generate a random number within range 0 - 6
 const random = Math.ceil(Math.random() * 6);
-
+// select the dice from dice folder and concantenate with the random number generated to .png string
 const dice = 'dice/dice' + random + '.png';
 
+// print the dice result to the console 
 // console.log(dice)
 
+// select all the first image
 const image = document.querySelectorAll('img')[0];
 
+// set the first image src to the variable dice value
 image.setAttribute('src', dice);
 
-// const image2 = document.querySelectorAll('img')[1];
-
-// image2.setAttribute('src', dice)
-
+// generate a random number & round to upper number
 const random2 = Math.ceil(Math.random()*6);
 
 const dice2 = 'dice/dice' + random2 + '.png';
-
+// select all the second image
 const image2 = document.querySelectorAll('img')[1];
-
+// set the second image src to the variable dice value
 image2.setAttribute('src', dice2);
 
 
 // this fires when the player1 value is greater than player2
 if (dice > dice2){
-    console.log('Player1 Wwwwins')
+    console.log('Player1 Wwwwins');
     heading.textContent = 'Player1 Wins';
     heading.style.color = 'aquamarine';
 }
